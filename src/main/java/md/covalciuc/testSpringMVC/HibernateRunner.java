b462c12f98c01abbb43554efb3c6ebb686d0144a
Ion Covalciuc <covalciuc00@gmail.com>
2023-10-09 22:04:16 +0300

    I have added new package entity, in this package is User class for add object in Table users. Using HibernateRunner I added first object in table users.

--- src/main/java/md/covalciuc/testSpringMVC/HibernateRunner.java
+++ src/main/java/md/covalciuc/testSpringMVC/HibernateRunner.java
@@ -1,11 +1,14 @@
 package md.covalciuc.testSpringMVC;
+import md.covalciuc.testSpringMVC.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.concurrent.BlockingDeque;
 
 public class HibernateRunner {
@@ -15,11 +18,24 @@ public class HibernateRunner {
 //        Connection connection = DriverManager
 //                .getConnection("db.url","db.username","db.password");
         Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(User.class);
+        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
         configuration.configure();
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()){
-            System.out.println("OK");
+            session.beginTransaction();
+
+            User user = User.builder()
+                    .username("ivan@hmail.com")
+                    .firstname("Ivan")
+                    .lastname("Ivanov")
+                    .birth_date(LocalDate.of(2000,1,19))
+                    .age(20)
+                    .build();
+            session.save(user);//I have added new object in my table users
+
+            session.getTransaction().commit();
         }
 
     }
